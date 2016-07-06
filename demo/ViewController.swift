7a2b479cc3307bab1e4a4f75e137310c903af2d6
歐涵臻 <Jenny@ouhanzhende-macbook-pro.local>
2016-07-06 19:53:54 +0800

    Initial Commit

---
+++ demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by 歐涵臻 on 2016/7/6.
+//  Copyright © 2016年 Jenny Ou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
